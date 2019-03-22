5cccd645bc0163b5f5d503d16a9e0a0f80d65b00
Mark Sherstan <mshersta@ualberta.ca>
2019-03-22 17:08:12 -0600

    Remove altVisual to simplify code.

--- Simple/servoSimple.py
+++ Simple/servoSimple.py
@@ -38,34 +38,6 @@ timeStamp = now.strftime("%Y-%m-%d_%H.%M.%S") + ".avi"
 
 out = cv2.VideoWriter(timeStamp,cv2.VideoWriter_fourcc('M','J','P','G'), 15, (width,height))
 
-# Displays the altitude on right side od screen as a linear representation
-def altVisual(color, altitude):
-    drawColor = (0,255,0)
-    locationX = 1150
-    upperY = 200
-    lowerY = 700
-    alt50 = upperY + 333 #upperY + (lowerY-upperY)*((150.0-50)/150.0)
-    alt100 = upperY + 167 #upperY + (lowerY-upperY)*((150.0-100)/150.0)
-
-    currentAlt = int(upperY + (lowerY-upperY)*((150.0-altitude)/150.0))
-
-    cv2.line(color,(locationX-25,currentAlt+25),(locationX,currentAlt),drawColor,2)
-    cv2.line(color,(locationX-25,currentAlt-25),(locationX,currentAlt),drawColor,2)
-    cv2.putText(color,str(altitude),(locationX-60,currentAlt+5),font,0.75,drawColor,2)
-
-    cv2.putText(color,"Alt",(locationX,upperY-25),font,0.75,drawColor,2)    # Title
-    cv2.line(color,(locationX,lowerY),(locationX,upperY),drawColor,1)       # Vertical Line
-
-    cv2.line(color,(locationX,lowerY),(locationX+25,lowerY),drawColor,2)    # Bottom Line
-    cv2.line(color,(locationX,upperY),(locationX+25,upperY),drawColor,2)    # Top Line
-    cv2.line(color,(locationX,alt50),(locationX+25,alt50),drawColor,2)      # 50 ft
-    cv2.line(color,(locationX,alt100),(locationX+25,alt100),drawColor,2)    # 100 ft
-
-    cv2.putText(color,"0 ft",(locationX+30,lowerY),font,0.5,drawColor,2)
-    cv2.putText(color,"50 ft",(locationX+30,alt50),font,0.5,drawColor,2)
-    cv2.putText(color,"100 ft",(locationX+30,alt100),font,0.5,drawColor,2)
-    cv2.putText(color,"150 ft",(locationX+30,upperY),font,0.5,drawColor,2)
-
 # Get telemetry data [ m/s, radians, radians, m ]
 def getFlightData():
     groundSpeed = vehicle.groundspeed
@@ -105,9 +77,6 @@ while(True):
     now = datetime.datetime.now()
     timeStamp = now.strftime("%Y-%m-%d %H:%M:%S")
     cv2.putText(color,timeStamp,(1000,20),font,fontScale1,fontColor,lineType)
-
-    # Display the linear altitude
-    altVisual(color, altitude)
 
     # Keyboard Toggles
     key = cv2.waitKey(1)
